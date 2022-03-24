81cf741c90ff0302e8729d678ac63cbed6cff79c
Guilherme-Vecchi <guivecchi12@gmail.com>
2022-03-24 16:26:51 -0600

    started on header

---
+++ single_page/pages/eide_bailly_suite.js
@@ -0,0 +1,98 @@
+import Head from 'next/head';
+
+export default function Eide(){
+    return (
+        <div>
+            <Head>
+                <title>Eide Bailly Suite</title>
+                <meta name="description" content="Eide Bailly Suite for Utah Jazz" />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <h1>Eide Bailly Suite Level</h1>
+            <p>
+                ABOUT
+                Location
+                Suite Level (Level 4)
+                Capacity
+                Accommodates 18–32 people
+                Event Availability
+                Utah Jazz Games (including playoffs)
+                Concerts and select special events
+            </p>
+            <p>
+                BENEFITS & AMENITIES
+                Private Suite
+                420–490 square feet depending on the location
+                Lounge seating area for four with coffee table at the back of suite
+                High-top counter in middle, and drink rails at the back of the fixed-seating area
+                Dedicated suite attendant for each game and arena event
+                Flat screen TV in each suite
+                WiFi access
+                18–32 tickets in a luxury suite (varies by location)
+                VIP parking passes
+                Food credit for catering to both Utah Jazz games and arena events
+            </p>
+            <p>
+                PARKING & CHECK-IN
+                VIP parking in the Park Place lot located right next to Vivint Smart Home Arena
+                Easy check-in: Talk to the host at any suite level (level 4) entrance to check-in 
+            </p>
+            <p>
+                POSSIBLE USES
+                Business Development
+                Client Entertainment
+                Relationship Development
+                Generate Referrals by Inviting Clients to Bring Colleagues & Friends
+                Client Retention
+                Renew Accounts
+                Reduce Attrition
+                Relationship Development
+                Current Customer Upsell
+                New Product/Service Launch
+                Employee Usage
+                Employee Recruitment & Retention
+                Employee Benefit Programs
+                Employee Incentives
+                Internal Contests
+                Team Building
+                Employee Families
+                Holiday Events/Parties
+                Conferences
+                Executive Utilization
+                Executive Team Building
+                Quarterly Meetings
+                Investors & Shareholders
+                Personal Entertainment
+                Community Relations
+                Complement Current Charitable Programs
+                Donate for Auctions or Raffles
+                Host 501(c)(3) Organizations
+            </p>
+            <p>
+                Tailor your food and beverage experience to your group each night. Choose from an expansive menu based on who you will be hosting for that event. Submit your food and beverage orders 2 business days prior to the event. Access to your suite and food service will start 90 minutes prior to tipoff
+            </p>
+            <p>
+                DIGITAL TICKETS
+                All tickets will be digital and accessible in the Utah Jazz + Vivint Arena App. Printed tickets will no longer be an option.
+            </p>
+            <p>
+                This move to digital-only has been made for several reasons:
+
+                Helps to prevent fraudulent tickets from being circulated
+                Increases safety and security by providing an easier way to track who’s using tickets and entering the arena
+                Allows for a convenient transfer process—you can transfer your tickets with the touch of a button
+                - Helps to prevent fraudulent tickets from being circulated.
+                - Increases safety and security by providing an easier way to track who’s using tickets and entering the arena.
+                - Allows for a convenient transfer process—you can transfer your tickets with the touch of a button.
+            </p>
+            <p>
+                If you have questions or concerns about digital tickets, please contact your account representative.
+
+                CONTACT US
+                801-325-2105
+                bizdev@utahjazz.com
+                With options like single-game tickets, suite rentals and special group rates, experiencing Utah Jazz basketball has never been more convenient! Just let us know what you’re interested in and we can customize a package to fit your group and budget.
+            </p>
+        </div>
+    )
+}
---
+++ single_page/pages/header.js
@@ -0,0 +1,12 @@
+export default function header(){
+    return(
+        <div className="flex justify-between w-full p-5 text-white bg-black">
+            <h1>UTAH JAZZ PREMIUM SEATING</h1>
+            <div className="flex justify-between w-1/5">
+                <p className="hover:font-bold">Luxury Suites</p>
+                <p className="hover:font-bold">Exclusive Clubs</p>
+                <p className="hover:font-bold">Contact Us</p>
+            </div>
+        </div>
+    )
+}
