f9a29fa471b7e8393c2d8023a1ee94465c330638
Guilherme-Vecchi <guivecchi12@gmail.com>
2022-03-29 15:52:43 -0600

    carousel

--- components/carousel.js
+++ components/carousel.js
@@ -1,10 +1,57 @@
-import React from 'react'
+import 'tw-elements';
 
 const Carousel = () => {
+    
     return(
-      <div 
-        className="w-full text-2xl font-bold bg-center bg-cover h-[25rem] bg-carousel" 
-      />  
+      <div id="carouselExampleCaptions" class="carousel slide relative w-full" data-bs-ride="carousel">
+        <div class="carousel-indicators absolute right-0 bottom-0 left-0 flex justify-center p-0 mb-4">
+          <button
+            type="button"
+            data-bs-target="#carouselExampleCaptions"
+            data-bs-slide-to="0"
+            class="rounded-[50%] active !h-[15px] !w-[15px] hover:bg-[#fffb07]"
+            aria-current="true"
+            aria-label="Slide 1"
+          ></button>
+          <button
+            type="button"
+            data-bs-target="#carouselExampleCaptions"
+            data-bs-slide-to="1"
+            aria-label="Slide 2"
+            class="rounded-[50%] active !h-[15px] !w-[15px] hover:bg-[#fffb07]"
+          ></button>
+          <button
+            type="button"
+            data-bs-target="#carouselExampleCaptions"
+            data-bs-slide-to="2"
+            aria-label="Slide 3"
+            class="rounded-[50%] active !h-[15px] !w-[15px] hover:bg-[#fffb07]"
+          ></button>
+        </div>
+        <div class="carousel-inner relative w-full overflow-hidden">
+          <div class="carousel-item active relative float-left w-full max-h-[500px]">
+            <img
+              src="https://images.unsplash.com/photo-1498837167922-ddd27525d352?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8N3x8Zm9vZHxlbnwwfDB8MHx8&auto=format&fit=crop&w=500&q=60"
+              class="block w-full"
+              alt="..."
+            />
+          </div>
+          <div class="carousel-item relative float-left w-full max-h-[500px]">
+            <img
+              src="https://images.unsplash.com/photo-1504674900247-0877df9cc836?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8Zm9vZHxlbnwwfDB8MHx8&auto=format&fit=crop&w=500&q=60"
+              class="block w-full"
+              alt="..."
+            />
+          </div>
+          <div class="carousel-item relative float-left w-full max-h-[500px]">
+            <img
+              src="https://images.unsplash.com/photo-1606787366850-de6330128bfc?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8Zm9vZHxlbnwwfDB8MHx8&auto=format&fit=crop&w=500&q=60"
+              class="block w-full"
+              alt="..."
+            />
+          </div>
+        </div>
+      </div>
     )
 }
 
