06b9165add67aa41a97bb5290ede8d9c4f346680
Guilherme-Vecchi <guivecchi12@gmail.com>
2022-03-31 11:42:47 -0600

    space

--- components/carousel.js
+++ components/carousel.js
@@ -3,11 +3,13 @@ import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { Carousel } from 'react-responsive-carousel';
 
 export default function NextJsCarousel() {
+  
   const images = [
     "https://images.unsplash.com/photo-1602357280104-742c517a1d82?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8c3BvcnQlMjBjb3VydHxlbnwwfDB8MHx8&auto=format&fit=crop&w=500&q=60",
     "https://images.unsplash.com/photo-1511415221243-04dab195b318?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8N3x8c3BvcnQlMjBjb3VydHxlbnwwfDB8MHx8&auto=format&fit=crop&w=500&q=60",
     "https://images.unsplash.com/photo-1474224017046-182ece80b263?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTZ8fHNwb3J0JTIwY291cnR8ZW58MHwwfDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
   ]
+
   return (
     <div className='flex justify-center w-full bg-gray-900'>
 			<Carousel 
